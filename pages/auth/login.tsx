2ab5faa49250a54339c1fb4248c013ca2cf157dd
Joaquin Caggiano <joaquincaggiano@gmail.com>
2023-01-28 12:51:39 -0300

    login page en proceso

---
+++ pages/auth/login.tsx
@@ -0,0 +1,12 @@
+import { MainLayout } from "@/components/layouts";
+
+
+const LoginPage = () => {
+  return (
+    <MainLayout title="Login" metaHeader="Iniciar sesión">
+        
+    </MainLayout>
+  )
+}
+
+export default LoginPage;
