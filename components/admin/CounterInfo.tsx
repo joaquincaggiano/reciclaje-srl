9c9ffc72d024e4f1e07c6bcbf5a567ae2d30309e
Joaquin Caggiano <joaquincaggiano@gmail.com>
2023-02-28 13:08:14 -0300

    dashboard styles fixed

--- components/admin/CounterInfo.tsx
+++ components/admin/CounterInfo.tsx
@@ -1,49 +1,55 @@
 import { FC } from "react";
 
-import { Card, CardContent, Grid, Typography } from "@mui/material";
+import NextLink from "next/link";
+
+import { Card, CardContent, Grid, Typography, Link } from "@mui/material";
 
 interface Props {
   title: string | number;
   subTitle: string;
   icon: JSX.Element;
+  url: string;
 }
 
-export const CounterInfo: FC<Props> = ({ title, subTitle, icon }) => {
+export const CounterInfo: FC<Props> = ({ title, subTitle, icon, url }) => {
   return (
     <Grid item xs={12} sm={4} md={3}>
-      <Card
-        sx={{
-          display: "flex",
-          justifyContent: "space-around",
-          alignItems: "center",
-          boxShadow: "0px 0px 0px 0px",
-        }}
-      >
-        <CardContent
-          sx={{
-            width: 50,
-          }}
-        >
-          {icon}
-        </CardContent>
+      <NextLink href={url} passHref legacyBehavior>
+        <Link>
+          <Card
+            sx={{
+              display: "flex",
+              justifyContent: "space-around",
+              alignItems: "center",
+              boxShadow: "0px 0px 0px 0px",
+            }}
+          >
+            <CardContent
+              sx={{
+                width: 50,
+              }}
+            >
+              {icon}
+            </CardContent>
 
-        <CardContent
-          sx={{
-            flex: "1 0 auto",
-            display: "flex",
-            alignItems: "baseline",
-            justifyContent: "space-evenly",
-          }}
-        >
-          <Typography variant="h3" sx={{ fontSize: "26px" }}>
-            {subTitle}:{" "}
-          </Typography>
-          <Typography variant="caption" sx={{ fontSize: "20px" }}>
-            {title}
-          </Typography>
-        </CardContent>
-        
-      </Card>
+            <CardContent
+              sx={{
+                flex: "1 0 auto",
+                display: "flex",
+                alignItems: "baseline",
+                justifyContent: "space-evenly",
+              }}
+            >
+              <Typography variant="h3" sx={{ fontSize: "26px" }}>
+                {subTitle}:{" "}
+              </Typography>
+              <Typography variant="caption" sx={{ fontSize: "20px" }}>
+                {title}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Link>
+      </NextLink>
     </Grid>
   );
 };
--- pages/admin/dashboard.tsx
+++ pages/admin/dashboard.tsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from "react";
 
-import NextLink from "next/link";
-
 import useSWR from "swr";
 
 import { CounterInfo } from "@/components/admin";
@@ -58,71 +56,43 @@ const DashboardPage = () => {
       title="Dashboard General"
       metaHeader="Mantenimiento general del dashboard"
     >
-      <Grid container display="flex" justifyContent="space-evenly" flexWrap="wrap" spacing={2}>
-        <Grid item sx={{ width: "100%" }}>
-          <NextLink href="/admin/products" passHref legacyBehavior>
-            <Link>
-              <CounterInfo
-                icon={<CategoryOutlined color="error" sx={{ fontSize: 40 }} />}
-                title={numbersOfProducts}
-                subTitle={"Productos"}
-              />
-            </Link>
-          </NextLink>
-        </Grid>
+      <Grid container spacing={2}>
+        <CounterInfo
+          icon={<CategoryOutlined color="error" sx={{ fontSize: 40 }} />}
+          title={numbersOfProducts}
+          subTitle={"Productos"}
+          url={"/admin/products"}
+        />
 
-        <Grid item sx={{ width: "100%" }}>
-          <NextLink href="/admin/services" passHref legacyBehavior>
-            <Link>
-              <CounterInfo
-                icon={
-                  <PrecisionManufacturing
-                    color="warning"
-                    sx={{ fontSize: 40 }}
-                  />
-                }
-                title={numbersOfServices}
-                subTitle={"Servicios"}
-              />
-            </Link>
-          </NextLink>
-        </Grid>
+        <CounterInfo
+          icon={
+            <PrecisionManufacturing color="warning" sx={{ fontSize: 40 }} />
+          }
+          title={numbersOfServices}
+          subTitle={"Servicios"}
+          url={"/admin/services"}
+        />
 
-        <Grid item sx={{ width: "100%" }}>
-          <NextLink href="/admin/blog" passHref legacyBehavior>
-            <Link>
-              <CounterInfo
-                icon={
-                  <NewspaperOutlined sx={{ fontSize: 40, color: "#3C99DC" }} />
-                }
-                title={numbersOfBlogs}
-                subTitle={"Blog"}
-              />
-            </Link>
-          </NextLink>
-        </Grid>
+        <CounterInfo
+          icon={<NewspaperOutlined sx={{ fontSize: 40, color: "#3C99DC" }} />}
+          title={numbersOfBlogs}
+          subTitle={"Blog"}
+          url={"/admin/blog"}
+        />
 
-        <Grid item sx={{ width: "100%" }}>
-          <NextLink href="/admin/users" passHref legacyBehavior>
-            <Link>
-              <CounterInfo
-                icon={<GroupOutlined sx={{ fontSize: 40, color: "#4caf50" }} />}
-                title={numbersOfSubscribes}
-                subTitle={"Suscriptores"}
-              />
-            </Link>
-          </NextLink>
-        </Grid>
+        <CounterInfo
+          icon={<GroupOutlined sx={{ fontSize: 40, color: "#4caf50" }} />}
+          title={numbersOfSubscribes}
+          subTitle={"Suscriptores"}
+          url={"/admin/users"}
+        />
 
-        <Grid item sx={{ width: "100%" }}>
-          <CounterInfo
-            icon={
-              <AccessTimeOutlined sx={{ fontSize: 40, color: "#3A64D8" }} />
-            }
-            title={refreshIn}
-            subTitle={"Refresh"}
-          />
-        </Grid>
+        <CounterInfo
+          icon={<AccessTimeOutlined sx={{ fontSize: 40, color: "#3A64D8" }} />}
+          title={refreshIn}
+          subTitle={"Refresh"}
+          url=""
+        />
       </Grid>
     </MainLayout>
   );
