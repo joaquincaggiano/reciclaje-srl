41af3bd246ea50c044a223879fb7f14ce1b331a5
Joaquin Caggiano <joaquincaggiano@gmail.com>
2023-04-08 18:20:07 -0300

    comentarios para solucionar el bug del middleware

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
+// Hay que traer de la base los title de los productos, servicios y blog
+
 export async function middleware(req: NextRequest) {
   const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   // console.log("SESSION", session);
@@ -23,5 +25,8 @@ export const config = {
     "/admin/services",
     "/admin/blog",
     "/admin/users",
+    "/admin/products/[title].tsx",
+    "/admin/services/[title].tsx",
+    "/admin/blog/[title].tsx",
   ],
 };
