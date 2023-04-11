d3fdf27184835cfb50a84338c130ffcd2c202e28
Joaquin Caggiano <joaquincaggiano@gmail.com>
2023-04-11 15:12:34 -0300

    middleware dynamic

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,8 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-// Hay que traer de la base los title de los productos, servicios y blog
-
 export async function middleware(req: NextRequest) {
   const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-  // console.log("SESSION", session);
 
   if (!session) {
     const url = req.nextUrl.clone();
@@ -19,14 +16,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    "/admin/dashboard",
-    "/admin/products",
-    "/admin/services",
-    "/admin/blog",
-    "/admin/users",
-    "/admin/products/[title].tsx",
-    "/admin/services/[title].tsx",
-    "/admin/blog/[title].tsx",
-  ],
+  matcher: ["/admin/:path*"],
 };
