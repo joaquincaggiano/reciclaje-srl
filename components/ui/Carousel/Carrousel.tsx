bf02dc50ddd83e01646e09643c39bf0e0a8a6934
Joaquin Caggiano <joaquincaggiano@gmail.com>
2023-04-24 13:16:20 -0300

    foto en el banner

--- components/ui/Carousel/Carrousel.tsx
+++ components/ui/Carousel/Carrousel.tsx
@@ -5,7 +5,7 @@ import { ArrowBackIosNewOutlined, ArrowForwardIosOutlined } from "@mui/icons-mat
 const images = [
   "https://todorecsrl-test-dev.s3.sa-east-1.amazonaws.com/banner/frente1.jpg",
   "https://todorecsrl-test-dev.s3.sa-east-1.amazonaws.com/banner/adentro4.jpg",
-  "https://todorecsrl-test-dev.s3.sa-east-1.amazonaws.com/banner/adentro1.jpg",
+  "https://todorecsrl-test-dev.s3.sa-east-1.amazonaws.com/banner/adentro2.jpg",
 ];
 
 export const Carrousel = () => {
